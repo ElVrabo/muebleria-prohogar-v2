48b26506a03be9635563fdee7897cc857f079b58
Brandonmkmk <brandonxd611@gmail.com>
2024-07-23 21:03:33 -0600

    refactoring: improve routes

--- src/config/routes/App.js
+++ src/config/routes/App.js
@@ -1,13 +1,12 @@
 import "../../index.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from "react-router-dom";
-import { CATALOG, CATALOGID, CHANGEPASSWORDPAGES, CONTACT, CREATEEMPLOYEESPAGES, CREATEEMPLOYEESSPAGES, DASHBOARD, EDITEMPLOYEESPAGES, EDITPROFILE, EMPLOYEESPAGES, ERRORPAY, FAVORITEPRODUCTS, FORMADDADDRESS, FORMADDPRODUCTS, FORMADMIN, FORMLOGIN, FORMPROVIDERPRODUCTSPAGES, FORMREGISTER, HOME, LISTPROVIDERPRODUCTS, LOGIN, LOGINADMIN, MANAGEPRODUCTS, PROFILE, REGISTER, SHOPPINGCART, SUCCESSPAY, SuccessPay } from "./path";
+import { CATALOG, CATALOGID, CHANGEPASSWORDPAGES, CONTACT, CREATEEMPLOYEESPAGES, CREATEEMPLOYEESSPAGES, EDITEMPLOYEESPAGES, EDITPRODUCTS, EDITPROFILE, EMPLOYEESPAGES, ERRORPAY, FORMADDPRODUCTS, FORMADMIN, FORMLOGIN, FORMPROVIDERPRODUCTSPAGES, FORMREGISTER, HOME, LISTPROVIDERPRODUCTS, LOGIN, LOGINADMIN, MANAGEPRODUCTS, PRODUCTSPAGES, PROFILE, REGISTER, SHOPPINGCART, SUCCESSPAY, SuccessPay } from "./path";
 import { Home } from "../../components/pages/Home/Home";
 import { UserContextProvider } from '../../context/usersContext';
 import { ProductsContextProvider } from '../../context/productsContext';
 import ProtectedRoutes from '../../ProtectedRoutes';
 import Profile from '../../components/pages/profile/Profile';
-import FavoriteProducts from '../../components/pages/profile/FavoriteProducts';
 import ProductsPages from "../../components/pages/AdminPages/products/ProductsPages.js";
 import FormAddProducts from '../../components/pages/AdminPages/products/FormAddProducts.js';
 import ProductsDetailsPages from '../../components/pages/CatalogProducts/ProductsDetailsPages.js';
@@ -15,7 +14,6 @@ import CatalogProductsPages from '../../components/pages/CatalogProducts/Catalog
 import CartPages from '../../components/pages/ProductsCart/CartPages.js';
 import EditProfilePages from '../../components/pages/profile/EditProfilePages';
 import ContactPages from '../../components/pages/Contact/ContactPages.js';
-import FormAddAdress from "../../components/pages/FormAddAddress/FormAddAdress.js";
 import LoginPages from "../../components/pages/AdminPages/login/LoginPages.js";
 import Login from "../../components/pages/Auth/Login.js";
 import Register from "../../components/pages/Auth/Register.js";
@@ -41,8 +39,8 @@ function App() {
       <Route path={REGISTER} element={<Register/>}/>
       <Route path={LOGINADMIN} element={<LoginPages/>} />
       <Route path={FORMADDPRODUCTS} element={<FormAddProducts/>} />
-      <Route path={MANAGEPRODUCTS} element={<ProductsPages/>} />
-      <Route path='/manageProducts/:id' element={<FormAddProducts/>} />
+      <Route path={PRODUCTSPAGES} element={<ProductsPages/>} />
+      <Route path={EDITPRODUCTS} element={<FormAddProducts/>} />
       <Route path={FORMPROVIDERPRODUCTSPAGES} element={<FormAddSellers/>} />
       <Route path={LISTPROVIDERPRODUCTS} element={<SellersPages/>} />
       <Route path={EMPLOYEESPAGES} element={<EmployeesPage/>} />
@@ -54,12 +52,11 @@ function App() {
       <Route path={HOME} element={<Home />} />
       <Route path={PROFILE} element={<Profile/>} />
       <Route path={EDITPROFILE} element={<EditProfilePages/>} />
-      <Route path={FAVORITEPRODUCTS} element={<FavoriteProducts/>} />
+      {/* <Route path={FAVORITEPRODUCTS} element={<FavoriteProducts/>} /> */}
       <Route path={CATALOG} element={<CatalogProductsPages/>} />
       <Route path={SHOPPINGCART} element={<CartPages/>} />
       <Route path={CATALOGID} element={<ProductsDetailsPages/>} />
       <Route path={CONTACT} element={<ContactPages/>} />
-      <Route path={FORMADDADDRESS} element={<FormAddAdress/>} />
       <Route path={CHANGEPASSWORDPAGES} element={<ChangePassword/>} />
       <Route path={SUCCESSPAY} element={<SuccessPayment/>} />
       <Route path={ERRORPAY} element={<ErrorPayment/>} />
