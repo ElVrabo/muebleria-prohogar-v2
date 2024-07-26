7227b0708f35b521eb169759a89f8756143e392d
Brandonmkmk <brandonxd611@gmail.com>
2024-07-25 19:44:22 -0600

    add- adding buttons to shopping cart

--- src/components/common/nav/Navigation.js
+++ src/components/common/nav/Navigation.js
@@ -10,6 +10,7 @@ import { productsContext } from "../../../context/productsContext";
 import Button from 'react-bootstrap/Button';
 import ModalComponent from "../modal/ModalComponent";
 import Navbar from 'react-bootstrap/Navbar';
+import { paymentRequest } from "../../../api/payment";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 const MySwal = withReactContent(Swal)
@@ -60,29 +61,64 @@ export const Navigation = () => {
           functionShow={showModal}
           funtionOnHide={handleClose}
           titleModal={"Carrito de compras"}
-          bodyModal={listProductsCart.length > 0 ? 
-            listProductsCart.map((p)=>(
-              <>
+          bodyModal={
+            <>
+              {listProductsCart.length > 0 ? (
+        <>
+          {listProductsCart.map((p) => (
+            <div key={p._id}>
               <section className="products-cart">
-              <img src={p.image} alt="imagen del producto" />
-              <div className="product-body" >
-              <h2>{p.name[0].toUpperCase() + p.name.slice(1).toLowerCase()}</h2>
-              <p>{p.description}</p>
-              <p>${p.price}</p>
-              <Button className="button-delete" variant="warning" onClick={async()=>{
-              await deleteProduct(p._id)
-               MySwal.fire({
-                title:'Se descarto correctamente',
-                icon:'success'
-               })
-               getProducts()
-              }} >Descartar</Button>
-              </div>
+                <img src={p.image} alt="imagen del producto" />
+                <div className="product-body">
+                  <h2>{p.name[0].toUpperCase() + p.name.slice(1).toLowerCase()}</h2>
+                  <p>{p.description}</p>
+                  <p>${p.price}</p>
+                  <div className="container-buttons-product" >
+                  <Button
+                    className="button-delete"
+                    variant="warning"
+                    onClick={async () => {
+                      await deleteProduct(p._id);
+                      MySwal.fire({
+                        title: 'Se descartó correctamente',
+                        icon: 'success',
+                      });
+                      getProducts();
+                    }}
+                  >
+                    Descartar
+                  </Button>
+                  <Button
+                    className="button-pay-a-product"
+                    variant="light"
+                    onClick={() => {
+                      paymentRequest(p).then((res) => (window.location.href = res.data.init_point));
+                    }}
+                  >
+                    Pagar este producto
+                  </Button>
+                  </div>
+                </div>
               </section>
-              <hr/>
-              </>
-            )
-          ) : <h5>No tienes ningun producto en el carrito</h5>
+              <hr />
+            </div>
+          ))}
+          <div className="container-button-pay-all-products" >
+          <Button
+            className="button-pay-all-products"
+            variant="light"
+            onClick={() => {
+              paymentRequest(listProductsCart).then((res)=>window.location.href=res.data.init_point)
+            }}
+          >
+            Ir a pagar todos los productos
+          </Button>
+          </div>
+        </>
+      ) : (
+        <h5>No tienes ningún producto en el carrito</h5>
+      )}
+            </>
           }
           />
           </div>
